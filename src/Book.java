dc550662ad8c1646236100b43d4597e8290c25b2
kdennewith <kdennewith@gmail.com>
2024-11-16 16:19:54 -0800

    Comments, small fixes.

--- src/Book.java
+++ src/Book.java
@@ -5,6 +5,15 @@ public class Book {
     private String IsbnNumber;
     private String callNumber;
 
+    /**
+     * This constructor will be used for storing books. We search for books using the callNumber since all are unique,
+     *  I read that ISBN numbers could be shared between book editions so that wouldn't work.
+     * @param title Title of book
+     * @param author Author of the book
+     * @param publicationYear Year the book was published
+     * @param isbnNumber Found on the lower portion of book above the barcode, a code given to every book, except reprints.
+     * @param callNumber Used to find books at certain locations in the library, found on spine it's an organization code.
+     */
     public Book(String title, String author, int publicationYear, String isbnNumber, String callNumber) {
         this.title = title;
         this.author = author;
--- src/Person.java
+++ src/Person.java
@@ -1,9 +1,37 @@
+import java.util.List;
+
 public class Person {
     private String name;
     private int studentID;
+    private List<DVD> checkedOutDVDs;
+    private List<Book> checkedOutBooks;
 
-    public Person(String name, int studentID) {
+    /**
+     * This constructor will be used to check student's current checked out books using their studentID's to search
+     * @param name Name of the student.
+     * @param studentID Students identification number assigned by school.
+     * @param checkedOutBooks List of currently checked out books
+     */
+    public Person(String name, int studentID, List<Book> checkedOutBooks, List<DVD> checkedOutDVDs) {
         this.name = name;
+        this.studentID = studentID;
+        this.checkedOutBooks = checkedOutBooks;
+        this.checkedOutDVDs = checkedOutDVDs;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(int studentID) {
         this.studentID = studentID;
     }
 }
